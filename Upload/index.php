7f61f5e517fb44cb5880c93a870ce19c0cf69bed
ciureaalex <webmaster@alex-site.url.p>
2015-08-14 21:57:13 +0300

    Update index.php
    The change will not be avalaible on the download page at the website

--- Upload/index.php
+++ Upload/index.php
@@ -6,7 +6,7 @@ if($maintenance!=$core){ ?>
 <h1 class="page-header">Index</h1><br><h3>Thank you for downloading SPlatform!<br>You can find more cool things at: <a href="http://splatform.ml">Splatform.ml</a><br>
 If you want to add pages, just follow this model:</h3><br>
 <iframe height=100% width=100% src="page.txt">
-<?php include("themes/default/header.theme.block.php"); ?>
+<?php include("themes/default/footer.theme.block.php"); ?>
 <?php
 }
 else{ include("config/maintenance-message.php");
